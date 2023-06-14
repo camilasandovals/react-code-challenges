39e1ed07b626fd616fd5f86b234a7d483eeca048
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-14 17:40:41 -0400

    finished

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState} from 'react'
+import "../App.css"
 
 const items = [{
   name: 'apple',
@@ -11,40 +12,91 @@ const items = [{
   price: 3.99
 }]
 
-function ShoppingCart () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
 
+function ShoppingCart () {
+  const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(0)
+  
+  const handleIncreaseQuantity = (item, index) => {
+    console.log("cart", cart)
+    setTotal(total + item.price)
+    setCart((prevCart) => {
+      console.log("prevcart", prevCart)
+      const updatedCart = [...prevCart];
+      console.log("destructured", updatedCart)
+      updatedCart[index].quantity += 1;
+      console.log("added", updatedCart)
+      return updatedCart;
+    });
+    
+  };
+  
+  const handleDecreaseQuantity = (item, itemIndex) => {
+    setTotal(total - item.price)
+    setCart((prevCart) => {
+      let updatedCart = [...prevCart];
+      if (updatedCart[itemIndex].quantity > 1) {
+        updatedCart[itemIndex].quantity -= 1;
+      } if (updatedCart[itemIndex].quantity === 1) {
+        updatedCart.splice(itemIndex, 1);
+      }
+      return updatedCart;
+    });
+    
+  };
+
+  const handleAddToCart = (item) => {
+    setTotal(total + item.price)
+    setCart((prevCart) => {
+      let updatedCart = [...prevCart];
+      const existingItemIndex = updatedCart.findIndex((cartItem) => cartItem.name === item.name);
+
+      if (existingItemIndex !== -1) {
+        updatedCart[existingItemIndex].quantity += 1;
+      return updatedCart;
+      }
+
+      else{
+        item.quantity = 1
+        updatedCart.push(item)
+        return updatedCart
+      }
+    }
+    )
+  }
+ 
   return (
     <div>
       <h1>Shopping Cart</h1>
       <div className='cart'>
         <div className='items'>
           <h2>Items</h2>
-          {items.map(item => (
+          {items.map((item, index) => (
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => handleAddToCart(item)}>Add to Cart</button>
             </div>)
           )}
         </div>
         <div>
           <h2>Cart</h2>
-          {cart.map(item => (
+          {cart.length>0? cart.map((item, index) => (
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+                <button onClick={() => handleDecreaseQuantity(item, index)}>-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => handleIncreaseQuantity(item, index)}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${(item.quantity * item.price).toFixed(2)}</p>
+
             </div>
-          ))}
+          )) : <p>Add items</p>}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${(total).toFixed(2)}</h2>
       </div>
     </div>
   )
