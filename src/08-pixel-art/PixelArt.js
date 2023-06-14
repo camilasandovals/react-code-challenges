bd5a07de35ab787b09b8cf1ab253f4890d1a4f56
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-14 13:40:39 -0400

    solved

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState, createContext, useContext } from 'react'
+
+const MyContext = createContext(null);
+
 
 function ColorPicker () {
+  const [selectedColor, setSelectedColor] = useContext(MyContext);
   const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map(color => <button onClick={() => {setSelectedColor(color); console.log(selectedColor)}} key={color} style={{ backgroundColor: color }} />)}
     </div>
   )
 }
 
 function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
+  const [selectedColor, setSelectedColor] = useContext(MyContext);
+  const [selectedPixel, setSelectedPixel] = useState("lightGrey")
+  return <div onClick={() => setSelectedPixel(selectedColor)} style={{ height: '20px', width: '20px', backgroundColor: selectedPixel, margin: '1px' }} />
 }
 
 function Pixels () {
@@ -25,10 +31,11 @@ function Pixels () {
 }
 
 export default function PixelArt () {
+  const [selectedColor, setSelectedColor] = useState("lightGrey")
   return (
-    <div>
-      <ColorPicker />
-      <Pixels />
-    </div>
+      <MyContext.Provider value={[selectedColor, setSelectedColor]}>
+        <ColorPicker />
+        <Pixels />
+      </MyContext.Provider>
   )
 }
