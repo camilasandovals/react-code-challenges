a41bd1f73d657c77039f462313dda05b45ecca4a
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-14 14:42:43 -0400

    finished

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,8 +1,28 @@
-const initialState = {}
+import { useReducer } from "react"
+const initialState = {
+  result : "no result yet",
+  num1: 0,
+  num2: 0 
+}
 
-function reducer (state, action) {}
+function reducer (state, action) {
+  switch(action.type){
+    case "num1":
+      return {...state, num1: action.number}
+    case "num2":
+      return {...state, num2: action.number}
+    case "+":
+      return {...state, result: state.num1 + state.num2}
+    case "-":
+      return {...state, result: state.num1 - state.num2}
+    case "c":
+      return {...state, result: 0, num1:0, num2: 0} 
+      //return initialState
+  }
+}
 
 export default function SimpleCalculator () {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   return (
@@ -10,24 +30,24 @@ export default function SimpleCalculator () {
       <div>
         <h2>Number 1</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button onClick={() => dispatch({type:"num1", number})}key={number}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Number 2</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button onClick={() => dispatch({type:"num2", number})} key={number}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Actions</h2>
-        <button>+</button>
-        <button>-</button>
-        <button>c</button>
+        <button onClick={() => dispatch({type:"+"})}>+</button>
+        <button onClick={() => dispatch({type:"-"})}>-</button>
+        <button onClick={() => dispatch({type:"c"})}>c</button>
       </div>
-      <div><h2>Result:</h2></div>
+      <div><h2>Result: {state.result}</h2> num1 : {state.num1}   |   num2 : {state.num2} </div>
     </div>
   )
 }
